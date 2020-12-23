dd3fd2dd30bd99d5d96eacfc017bdc8632e8fb20
Alex <sassadasas@gmail.com>
2020-12-23 21:52:56 +0200

    database update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, g, render_template
+from flask import Flask, g, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -20,6 +20,7 @@ def get_db():
     return g.db
 
 
+# noinspection PyUnusedLocal
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'db'):
@@ -29,12 +30,28 @@ def close_db(error):
 @app.route('/test')
 def test():
     people = []
+    q = request.args.get('q', default='', type=str)
     c = get_db().cursor()
-    for row in c.execute('SELECT * FROM test;'):
+    qs = '{}%'.format(q)
+    for row in c.execute(
+            '''SELECT *, first_name||' '||last_name AS full_name
+            FROM test 
+            WHERE (first_name LIKE ?) OR (last_name LIKE ?);''',
+            (qs, qs)):
         people.append({
             'id': row[0],
             'first_name': row[1],
-            'last_name': row[2]
-
+            'last_name': row[2],
+            'full_name': row[3]
         })
-    return render_template('test.html', people=people)
+    return render_template('test.html', people=people, q=q)
+
+
+@app.route('/test/add', methods=['POST'])
+def test_add():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    c = get_db().cursor()
+    c.execute('INSERT INTO test (first_name, last_name) VALUES (?,?)', (first_name, last_name))
+    get_db().commit()
+    return redirect('/test')
